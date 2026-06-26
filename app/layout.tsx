fbfb03076239bf1f9cc30784c1f6c2ee79035bdb
priyanshwho <priyanshu82711@gmail.com>
2026-06-26 16:44:09 +0530

    chore: change default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <QueryProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
